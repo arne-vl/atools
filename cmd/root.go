06d0e2ceb3112671e9ee3b1bb706f681cd206dfb
Arne Van Looveren <arne.vanlooveren@epicdatacronos.onmicrosoft.com>
2025-03-27 13:33:41 +0100

    Update release workflow

--- cmd/root.go
+++ cmd/root.go
@@ -7,7 +7,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const version = "0.1.2"
+const version = "dev"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
