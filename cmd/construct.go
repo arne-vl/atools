cc34147e8889d2b33cb00ec73bf4c1eefbe48f52
Arne Van Looveren <arne.vanlooveren@epicdatacronos.onmicrosoft.com>
2025-04-02 15:09:41 +0200

    Option to use variables

--- cmd/construct.go
+++ cmd/construct.go
@@ -1,17 +1,22 @@
 package cmd
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
 )
 
 var silent bool // Flag to silent "created" print
+
+var presetVariables = []string{"year", "quarter", "month", "monthnumber", "weeknumber", "day", "daynumber", "hour", "minute"}
 
 type Config struct {
 	Construction struct {
@@ -69,28 +74,119 @@ func parseYAMLFile(filename string) (*Config, error) {
 	return &config, nil
 }
 
-func constructFilesAndDirs(config *Config) error {
-	for _, dir := range config.Construction.Directories {
-		if err := os.MkdirAll(dir, os.ModePerm); err != nil {
-			return fmt.Errorf("failed to create directory %s: %w", dir, err)
+func stringInArray(arr []string, str string) bool {
+	for _, v := range arr {
+		if v == str {
+			return true
 		}
-		if !silent {
-			fmt.Println("Created directory:", dir)
+	}
+	return false
+}
+
+func promptForVariables(content string) map[string]string {
+	variables := make(map[string]string)
+	reader := bufio.NewReader(os.Stdin)
+
+	templateTags := map[string]struct{}{}
+	lines := strings.Split(content, "\n")
+
+	for _, line := range lines {
+		for {
+			start := strings.Index(line, "{{")
+			end := strings.Index(line, "}}")
+
+			if start == -1 || end == -1 {
+				break
+			}
+
+			variable := line[start+2 : end]
+			templateTags[strings.TrimSpace(variable)] = struct{}{}
+			line = line[:start] + line[end+2:]
 		}
 	}
 
+	for variable := range templateTags {
+		currentTime := time.Now()
+
+		if !stringInArray(presetVariables, variable) {
+			fmt.Printf("Enter value for %s: ", variable)
+			val, _ := reader.ReadString('\n')
+			variables[variable] = strings.TrimSpace(val)
+			if !silent {
+				fmt.Println()
+			}
+		} else {
+			switch variable {
+			case "year":
+				variables["year"] = fmt.Sprintf("%d", currentTime.Year())
+			case "quarter":
+				month := currentTime.Month()
+				quarter := (int(month)-1)/3 + 1
+				variables["quarter"] = fmt.Sprintf("Q%d", quarter)
+			case "month":
+				variables["month"] = currentTime.Month().String()
+			case "monthnumber":
+				monthNumber := int(currentTime.Month())
+				variables["monthnumber"] = fmt.Sprintf("%02d", monthNumber)
+			case "weeknumber":
+				dayOfYear := currentTime.YearDay()
+				weekNumber := (dayOfYear-1)/7 + 1
+				variables["weeknumber"] = fmt.Sprintf("%02d", weekNumber)
+			case "day":
+				variables["day"] = currentTime.Weekday().String()
+			case "daynumber":
+				variables["daynumber"] = fmt.Sprintf("%02d", currentTime.Day())
+			case "hour":
+				variables["hour"] = fmt.Sprintf("%02d", currentTime.Hour())
+			case "minute":
+				variables["minute"] = fmt.Sprintf("%02d", currentTime.Minute())
+			}
+		}
+	}
+
+	return variables
+}
+
+func applyBlueprint(content string, variables map[string]string) string {
+	for variable, value := range variables {
+		placeholder := fmt.Sprintf("{{ %s }}", variable)
+		content = strings.ReplaceAll(content, placeholder, value)
+	}
+
+	return content
+}
+
+func constructFilesAndDirs(config *Config) error {
+	var allContent []string
+	allContent = append(allContent, config.Construction.Directories...)
 	for _, file := range config.Construction.Files {
-		fileDir := filepath.Dir(file.Path)
+		allContent = append(allContent, file.Path)
+		allContent = append(allContent, file.Content)
+	}
+
+	variables := promptForVariables(strings.Join(allContent, "\n"))
+
+	for _, dir := range config.Construction.Directories {
+		processedDir := applyBlueprint(dir, variables)
+		if err := os.MkdirAll(processedDir, os.ModePerm); err != nil {
+			return fmt.Errorf("failed to create directory %s: %w", processedDir, err)
+		}
+		fmt.Println("Created directory:", processedDir)
+	}
+
+	for _, file := range config.Construction.Files {
+		processedPath := applyBlueprint(file.Path, variables)
+		fileDir := filepath.Dir(processedPath)
 		if err := os.MkdirAll(fileDir, os.ModePerm); err != nil {
-			return fmt.Errorf("failed to create directory for file %s: %w", file.Path, err)
+			return fmt.Errorf("failed to create directory for file %s: %w", processedPath, err)
 		}
 
-		if err := os.WriteFile(file.Path, []byte(file.Content), 0644); err != nil {
-			return fmt.Errorf("failed to create file %s: %w", file.Path, err)
+		processedContent := applyBlueprint(file.Content, variables)
+
+		if err := os.WriteFile(processedPath, []byte(processedContent), 0644); err != nil {
+			return fmt.Errorf("failed to create file %s: %w", processedPath, err)
 		}
-		if !silent {
-			fmt.Println("Created file:", file.Path)
-		}
+		fmt.Println("Created file:", processedPath)
 	}
 
 	return nil
@@ -102,14 +198,9 @@ var constructCmd = &cobra.Command{
 	Short: "Construct your blueprints",
 
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) == 0 {
-			fmt.Println("Please provide a blueprint to construct")
-			os.Exit(1)
-		}
-
 		filename, err := getConfigPath(args[0])
 		if err != nil {
-			fmt.Println("Error locating blueprint:", err)
+			fmt.Println("Error locating config file:", err)
 			os.Exit(1)
 		}
 
