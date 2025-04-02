afbd88b31e83223fdcb84f0298239565a902a397
Arne Van Looveren <arne.vanlooveren@epicdatacronos.onmicrosoft.com>
2025-04-02 08:58:35 +0200

    Basic construct command

---
+++ cmd/construct.go
@@ -0,0 +1,120 @@
+package cmd
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Construction struct {
+		Directories []string `yaml:"directories"`
+		Files       []File   `yaml:"files"`
+	} `yaml:"construction"`
+}
+
+type File struct {
+	Path    string `yaml:"path"`
+	Content string `yaml:"content,omitempty"`
+}
+
+func getConfigDir() string {
+	var homeDir string
+	if runtime.GOOS == "windows" {
+		homeDir = os.Getenv("USERPROFILE")
+	} else {
+		homeDir = os.Getenv("HOME")
+	}
+	return filepath.Join(homeDir, ".config", "atools", "blueprints")
+}
+
+func getConfigPath(filename string) (string, error) {
+	configDir := getConfigDir()
+	possibleFiles := []string{
+		filepath.Join(configDir, filename),
+		filepath.Join(configDir, filename+".yaml"),
+		filepath.Join(configDir, filename+".yml"),
+	}
+
+	for _, file := range possibleFiles {
+		if _, err := os.Stat(file); err == nil {
+			return file, nil
+		}
+	}
+
+	return "", fmt.Errorf("blueprint not found: %s", filename)
+}
+
+func parseYAMLFile(filename string) (*Config, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	data = bytes.ReplaceAll(data, []byte("\t"), []byte("    "))
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func constructFilesAndDirs(config *Config) error {
+	for _, dir := range config.Construction.Directories {
+		if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+			return fmt.Errorf("failed to create directory %s: %w", dir, err)
+		}
+		fmt.Println("Created directory:", dir)
+	}
+
+	for _, file := range config.Construction.Files {
+		fileDir := filepath.Dir(file.Path)
+		if err := os.MkdirAll(fileDir, os.ModePerm); err != nil {
+			return fmt.Errorf("failed to create directory for file %s: %w", file.Path, err)
+		}
+
+		if err := os.WriteFile(file.Path, []byte(file.Content), 0644); err != nil {
+			return fmt.Errorf("failed to create file %s: %w", file.Path, err)
+		}
+		fmt.Println("Created file:", file.Path)
+	}
+
+	return nil
+}
+
+// constructCmd represents the construct command
+var constructCmd = &cobra.Command{
+	Use:   "construct",
+	Short: "Construct your blueprints",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		filename, err := getConfigPath(args[0])
+		if err != nil {
+			fmt.Println("Error locating blueprint:", err)
+			os.Exit(1)
+		}
+
+		config, err := parseYAMLFile(filename)
+		if err != nil {
+			fmt.Println("Error parsing YAML:", err)
+			os.Exit(1)
+		}
+
+		if err := constructFilesAndDirs(config); err != nil {
+			fmt.Println("Error constructing directories and files:", err)
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(constructCmd)
+}
--- go.mod
+++ go.mod
@@ -2,11 +2,10 @@ module github.com/arne-vl/atools
 
 go 1.24.1
 
-require (
-	github.com/spf13/cobra v1.9.1
-)
+require github.com/spf13/cobra v1.9.1
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
+	gopkg.in/yaml.v3 v3.0.1
 )
