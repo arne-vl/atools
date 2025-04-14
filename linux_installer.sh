a00682445a32fc209f6e46bfe485ae134644036d
Arne Van Looveren <arne-vl@hotmail.be>
2025-04-14 21:20:07 +0200

    Fix: Update example file path in installer configuration

--- linux_installer.sh
+++ linux_installer.sh
@@ -77,7 +77,7 @@ blueprint:
   directories:
     - example
   files:
-    - name: example.txt
+    - name: example/example.txt
       content: |
         This is an example file.
         You can use this file to test atools.
