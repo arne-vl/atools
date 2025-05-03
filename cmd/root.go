a177db4dc59a6ba62bc2155a97dfb9f9aca76dd2
Arne Van Looveren <58698639+arne-vl@users.noreply.github.com>
2025-05-03 21:32:20 +0200

    chore: bump version (#72)

--- cmd/root.go
+++ cmd/root.go
@@ -7,7 +7,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const version = "0.4.1"
+const version = "0.4.2"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
